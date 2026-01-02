8a11aef8ef85526d64d99fb3f3f0ef4f01568165
anitach2003 <123134603+anitach2003@users.noreply.github.com>
2026-01-02 18:59:21 +0330

    Update raildb.py

--- configs/raildb.py
+++ configs/raildb.py
@@ -1,5 +1,5 @@
 dataset = 'raildb'
-data_root = '/kaggle/working/Rail-Detection/Rail-DB/'
+data_root = '/kaggle/working/updated/Rail-DB/'
 
 # TRAIN
 epoch = 100
@@ -23,15 +23,15 @@ cls_num_per_lane = 52
 # EXP
 note = 'test'
 
-log_path = '/kaggle/working/Rail-Detection/Rail-DB/utils'
+log_path = '/kaggle/working/updated/Rail-DB/utils'
 
 # FINETUNE or RESUME MODEL PATH
 finetune = None
 resume = None
 
 # TEST
-test_model = '/kaggle/working/Rail-Detection/Rail-DB/log/test_model.pth'
-test_work_dir = '/kaggle/working/Rail-Detection/Rail-DB/test/'
+test_model = '/kaggle/working/updated/Rail-DB/log/test_model.pth'
+test_work_dir = '/kaggle/working/updated/Rail-DB/test/'
 
 num_lanes = 4
 type = 'all'
